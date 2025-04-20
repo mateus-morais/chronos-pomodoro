7cdba44b37e78dab3514470df9f5c46c044f2cc2
Mateus Morais <mmorais385@gmail.com>
2025-04-19 22:55:56 -0300

    add DefaultInput component and destructure props

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DefaultInput';
 
 import './styles/theme.css';
 import './styles/global.css';
+
 
 
 export function App() {
@@ -20,6 +22,29 @@ export function App() {
 
       <Container>
         <CountDown />
+      </Container>
+
+      <Container>
+        <form className='form' action="">
+          <div className='formRow'>
+            <DefaultInput id='Input' type='text' />
+          </div>
+
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0 0 0</p>
+          </div>
+
+
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+
+        </form>
       </Container>
     </>
   );
