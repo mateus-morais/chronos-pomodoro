b242545c74955fd9fa1cbea95112224f33a1a7fc
Mateus Morais <mmorais385@gmail.com>
2025-04-19 22:02:20 -0300

    add logo e menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import { Heading } from "./components/Heading";
-import { Container } from "./components/Container";
+import { Container } from './components/Container';
+import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
 
-import "./styles/theme.css";
-import "./styles/global.css";
+import './styles/theme.css';
+import './styles/global.css';
 
 export function App() {
   return (
     <>
       <Container>
-        <Heading>Logo</Heading>
+        <Logo />
       </Container>
 
       <Container>
-        <Heading>Menu</Heading>
+        <Menu />
       </Container>
     </>
   );
--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,11 +1,11 @@
-import { TimerIcon } from "lucide-react";
-import styles from "./styles.module.css";
+import { TimerIcon } from 'lucide-react';
+import styles from './styles.module.css';
 
 export function Logo() {
   return (
     <div className={styles.logo}>
       <a className={styles.logoLink} href="#">
-        <TimerIcon />
+        <TimerIcon size={64} />
         <span>Chronos</span>
       </a>
     </div>
