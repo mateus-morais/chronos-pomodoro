6e048e1f50975f1cf3ce33e0b3f45138dcf4f2b7
Mateus Morais <mmorais385@gmail.com>
2025-04-19 21:03:44 -0300

    timericon lucide and  chronos text

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import "./styles/theme.css";
-import "./styles/global.css";
-
 import { Heading } from "./components/Heading";
 import { Container } from "./components/Container";
+
+import "./styles/theme.css";
+import "./styles/global.css";
 
 export function App() {
   return (
--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,5 +1,13 @@
+import { TimerIcon } from "lucide-react";
 import styles from "./styles.module.css";
 
 export function Logo() {
-  return <div className={styles.logo}>Logo</div>;
+  return (
+    <div className={styles.logo}>
+      <a href="#">
+        <TimerIcon />
+        <span>Chronos</span>
+      </a>
+    </div>
+  );
 }
