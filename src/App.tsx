ead73a4b8104a34648cdacabe16b7576bfba946a
Mateus Morais <mmorais385@gmail.com>
2025-04-22 16:47:24 -0300

    create folders MainForm, MainTemplate and Pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,11 @@ import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
-import { DefaultInput } from './components/DefaultInput';
-import { DefaultButton } from './components/DefaultButton';
-import { Cycles } from './components/Cycles';
-import { PlayCircleIcon } from 'lucide-react';
 import { Footer } from './components/Footer';
+import { MainForm } from './components/MainForm';
 
 import './styles/theme.css';
 import './styles/global.css';
-
-
-
 
 
 export function App() {
@@ -31,30 +25,7 @@ export function App() {
       </Container>
 
       <Container>
-        <form className='form' action="">
-          <div className='formRow'>
-            <DefaultInput
-              id='Input'
-              type='text'
-              labelText='task'
-              placeholder='Digite algo'
-            />
-          </div>
-
-          <div className='formRow'>
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-
-          <div className='formRow'>
-            <Cycles />
-          </div>
-
-
-          <div className='formRow'>
-            <DefaultButton icon={<PlayCircleIcon />} />
-          </div>
-
-        </form>
+        <MainForm />
       </Container>
 
       <Container>
