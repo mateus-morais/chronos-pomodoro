e960ca5ac3286a7801fbf5c878455c769887c0af
Mateus Morais <mmorais385@gmail.com>
2025-04-23 11:59:20 -0300

    add about pomodoro and not found page

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,12 @@
-import { Container } from './components/Container';
-import { Logo } from './components/Logo';
-import { Menu } from './components/Menu';
-import { CountDown } from './components/CountDown';
-import { Footer } from './components/Footer';
-import { MainForm } from './components/MainForm';
+import { Home } from './pages/Home';
+//import { AboutPomodoro } from './pages/AboutPomodoro';
+//import { NotFound } from './pages/NotFound';
 
 import './styles/theme.css';
 import './styles/global.css';
 
-
 export function App() {
   return (
-    <>
-      <Container>
-        <Logo />
-      </Container>
-
-      <Container>
-        <Menu />
-      </Container>
-
-      <Container>
-        <CountDown />
-      </Container>
-
-      <Container>
-        <MainForm />
-      </Container>
-
-      <Container>
-        <Footer />
-      </Container>
-    </>
+    <Home />
   );
 }
