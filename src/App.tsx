620df81a51b62ad61fad67ed87a6045e756c38e2
Mateus Morais <mmorais385@gmail.com>
2025-04-26 21:16:53 -0300

    added and organized contexts

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { Home } from './pages/Home';
+import { TaskContextProvider } from './contexts/TaskContext/TaskContextProvider';
 //import { AboutPomodoro } from './pages/AboutPomodoro';
 //import { NotFound } from './pages/NotFound';
 
 import './styles/theme.css';
 import './styles/global.css';
 
+
 export function App() {
   return (
-    <Home />
+    <TaskContextProvider>
+      <Home />
+    </TaskContextProvider>
   );
 }
